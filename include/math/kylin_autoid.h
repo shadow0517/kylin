035ef324785db6db5d5e39ff8ad15475dce331cc
shadow_liu <jliuchunhua@qq.com>
2017-10-23 17:05:08 +0800

    math autoid api add

--- include/math/kylin_autoid.h
+++ include/math/kylin_autoid.h
@@ -10,8 +10,16 @@
 struct kylin_autoid;
 typedef struct kylin_autoid kautoid_t;
 
+extern kautoid_t *kylin_autoid_create(uint32_t);
+extern void kylin_autoid_destroy(kautoid_t *);
 
+extern void kylin_autoid_clear(kautoid_t *);
 
+extern int kylin_autoid_total_count(kautoid_t *);
+extern int kylin_autoid_unset_count(kautoid_t *);
 
+extern int kylin_autoid_get(kautoid_t *);
+extern kerr_t kylin_autoid_put(kautoid_t *, uint32_t);
+extern kerr_t kylin_autoid_set(kautoid_t *, uint32_t);
 
 #endif /*_KYLIN_MATH_AUTOID_H_*/
